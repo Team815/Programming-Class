7202c77fd45b20a13a178ff190a1a76e72c1f03d
Cobb <ACOBB6@ford.com>
2018-08-13 15:33:27 -0400

    Added Lesson 2 material

---
+++ Lesson 2/Lesson_Materials/Main.java
@@ -0,0 +1,125 @@
+package Lesson_Materials;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		/* Review
+			// Comments
+				
+			// Naming conventions
+				// Pascal Case for ____
+				// Camel Case for ____
+				 
+			// Data types
+				// Numeric Data types
+				// Booleans
+				// Char and String
+				 
+			// Assignment Operators
+				// Basic assignment with '='
+				// Math operators
+				// Math assignment operators
+				 
+			// Explicit Casting
+			 
+			// I/O
+				// Input with Scanner
+				// Output with System.out
+		*/
+		
+		/* Conditional Statements */
+		
+			/*
+			// A simple use case
+			Scanner scanner = new Scanner(System.in);
+					
+			double dividend = scanner.nextDouble();
+			double divisor = scanner.nextDouble();
+			double quotient;
+			
+			if (divisor != 0) {
+				quotient = dividend/divisor;
+				System.out.println(quotient);
+			}
+			else System.out.println("Cannot divide by zero");
+			  
+			 // Conditional keywords
+			 	// if
+			 	// else
+			 	// else if
+			  
+			 // Conditional syntax
+			 	/* if ([VALUE] [CONDITIONAL OPERATOR] [VALUE]) {
+			 			[ACTION]
+			 		}
+			 	*/
+		
+				/* else {
+						[ACTION]
+					}
+				*/
+		
+				/* else if ([VALUE] [CONDITIONAL OPERATOR] [VALUE]) {
+			 			[ACTION]
+			 		}
+			 	*/
+			  
+			 // Conditional operators
+				// if equal to: ==
+				// if not equal to: !=
+				// if greater than: >
+				// if greater than or equal to: >=
+				// if less than: <
+				// if less than or equal to: <=
+				// Logical OR: ||
+				// Logical AND: &&
+			   
+			 // Switch statements
+				/* switch (VALUE) {
+					case VALUE: 
+					 ACTION;
+					 break;	
+				 */
+		
+		/* Loops */
+		
+			/*
+			 // A simple use case
+			 	Scanner scanner = new Scanner(System.in);
+					
+				double dividend = scanner.nextDouble();
+				double divisor = scanner.nextDouble();
+				double quotient;
+				
+				while (divisor == 0) {
+					System.out.println("Cannot use 0 as the divisor, please enter another divisor");
+					divisor = scanner.nextDouble();
+				}
+				quotient = dividend/divisor;
+				System.out.println(quotient);
+			  
+			 // Types of loops
+			 	// For
+			 	// While
+			 	// Do While
+			  
+			 // Loop keywords
+			 	// 'While' loop
+			 	// 'Do While' loop
+			 	// 'For' loop
+			 	// 'Foreach' loop
+			  
+			 // Loop syntax
+			 	// for(int i=0;i<VALUE;i++) {}
+			 	// for(NEW_VARIABLE : COLLECTION) {}
+			 	// while(CONDITION) {}
+			 	// do {} while (CONDITION)
+			 
+			 // Infinite loops
+			 */
+
+	}
+
+}
